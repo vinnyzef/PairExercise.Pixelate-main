1c8e8435325c3edce816145ea138cec6fdc94c8f
vinnyzef <72477005+vinnyzef@users.noreply.github.com>
2022-01-26 19:34:24 -0600

    working

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-console.log("hello world")
-
-console.log("new thing")
-
-console.log("additional thing")
+const tab = document.getElementById('table');
 
 
-console.log("what");
+function makeRow() {
+  let row = document.createElement("tr");
+
+
+  for (let i = 0; i < 20; i++) {
+    let td = document.createElement("td");
+    row.appendChild(td);
+  }
+
+  tab.appendChild(row);
+}
+
+document.getElementById('add-row').addEventListener('click', function () {
+  makeRow();
+
+});
+
+
+// document.querySelector('#table td').addEventListener('click', function () {
+// document.querySelector('#table td').style.backgroundColor = 'red';
+
+// });
+function changeBackground(color) {
+  let row = document.createElement("tr");
+  let td = document.createElement("td");
+  document.getElementById('#table td').style.background = color;
+}
+
+document.getElementById('td').addEventListener("click",function() { changeBackground('red') });
